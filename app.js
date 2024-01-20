9b6925e85263a58dd2f992e8aaca17cdc868fae3
Santiago_Calle <cmsantiago32@gmail.com>
2024-01-20 00:25:29 -0500

    Ejemplo de con else y plantillas de strings

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
+//Variables
 let numeroSecreto = 6;
-let numeroUsuario = prompt("Me indicas un número por favor:");
+let numeroUsuario = prompt("Me indicas un número entre 1 y 10 por favor:");
 
 console.log(numeroUsuario);
-
+/*
+Este código realiza
+la comparación
+*/
 if (numeroUsuario == numeroSecreto) {
-    alert('Acertaste el número');
+    //Acertamos, fue verdadera la condición
+    alert(`Acertaste, el número es: ${numeroUsuario}`);
+} else {
+    //La condición no se cumplió
+    alert('Lo siento, no acertaste el número');
 }
