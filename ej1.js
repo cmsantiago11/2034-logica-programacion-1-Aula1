c57989776b7e3a957af78256252adc5a34ff5716
Santiago_Calle <cmsantiago32@gmail.com>
2024-01-19 23:52:08 -0500

    Uso let, alarm e if

---
+++ ej1.js
@@ -0,0 +1,13 @@
+alert("¡Bienvenida y bievenido a nuestro sitio web!");
+let nombre = "Luna";
+let edad = 25;
+let numeroDeVentas = 50;
+let saldoDisponible = 1000;
+alert("Error! Completa todos los campos");
+let mensajeDeError = "Error! Completa todos los campos";
+alert(mensajeDeError);
+let nombreDeUsuario = prompt("Por favor, ingresa tu nombre");
+let edadDeUsuario = prompt("Por favor, ingresa tu edad");
+if (edadDeUsuario >= 18) {
+    alert("¡Puedes obtener tu licencia de conducir!");
+}
