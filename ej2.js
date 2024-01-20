fe34271a1f41bf93839b1e8effafac387541b451
Santiago_Calle <cmsantiago32@gmail.com>
2024-01-20 13:12:02 -0500

    ejemplo 2

---
+++ ej2.js
@@ -0,0 +1,6 @@
+console.log("Hola, bienvenido")
+let nombre = "Santiago";
+console.log(`Hola, ${nombre}!`);
+alert(`Hola, ${nombre}!`);
+lenguajeProgramacion = prompt("¿Cuál es tu lenguaje de programación favorito?");
+console.log(`Tu lenguaje de programación favorito es ${lenguajeProgramacion}`);
